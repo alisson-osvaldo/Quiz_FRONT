ca37aebe2e8d432e6bae11759ecaec7579bef45a
“Natacha <natachawistuba@gmail.com>
2021-11-24 19:39:01 -0300

    jogar

--- src/app/componentes/views/login/login/login.component.ts
+++ src/app/componentes/views/login/login/login.component.ts
@@ -1,4 +1,9 @@
+import { Login } from './../../../../models/login';
+import { ActivatedRoute, Router } from '@angular/router';
+import { JogadorService } from './../../../../services/jogador.service';
+import { LoginService } from './../../../../services/login.service';
 import { Component, OnInit } from '@angular/core';
+import { Jogador } from 'src/app/models/jogador';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    id!:            number;
     email!:      string;
     senha!:      string;
+    jogadores!: Jogador[ ];
+    jogadorId!: number;
 
-  constructor() { }
+  constructor(
+      private loginService: LoginService,
+      private jogadorService: JogadorService,
+      private router: Router,
+      private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {}
 
-  entrar( ): void {
-      //this.router.navigate([ranking/listar]); ainda não existe
-  }
+  entrar(): void {
+    let login: Login = {
+        email:       this.email,
+        senha:       this.senha,
+        jogadorId: this.jogadorId
+    };
+    this.loginService.create(login).subscribe((login) => {
+        console.log(login);
+        this.router.navigate(["jogador/listar"]);
+    });
+}
 
 }
